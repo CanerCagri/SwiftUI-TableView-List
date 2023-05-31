56b7ea3d36176483c51e6345a44de5756396f5f0
CanerCagri <canerrcagrii@gmail.com>
2023-05-31 15:24:13 +0300

    Initial Commit

---
+++ SwiftUI-TableView-List/SwiftUI_TableView_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TableView_ListApp.swift
+//  SwiftUI-TableView-List
+//
+//  Created by Caner Çağrı on 31.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TableView_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
